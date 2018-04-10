9aeba8472686b539481ae6316f59ba67db5d0ad7
Alexander Kluth <deralex@cpan.org>
2018-04-10 11:37:27 +0200

    Add another box with information
    Signed-off-by: Alexander Kluth <deralex@cpan.org>

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"os/exec"
 	"time"
 
-	ui "github.com/airking05/termui"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
+
+	ui "github.com/airking05/termui"
 )
+
+func getDockerVersion() string {
+	cmd := exec.Command("docker", "version")
+	stdout, _ := cmd.Output()
+
+	return string(stdout)
+}
 
 func main() {
 	cli, err := client.NewEnvClient()
@@ -43,16 +52,25 @@ func main() {
 
 	docker_ps_table := ui.NewTable()
 	docker_ps_table.Rows = container_table_rows
-	docker_ps_table.Y = 0
-	docker_ps_table.X = 0
 	docker_ps_table.Width = ui.TermWidth()
 	docker_ps_table.Height = ui.TermHeight()
 	docker_ps_table.BorderLabel = "Containers"
 	docker_ps_table.BorderLabelFg = ui.ColorWhite
 	docker_ps_table.BorderFg = ui.ColorGreen
+	docker_ps_table.Separator = false
+
+	docker_ps_table.Analysis()
+	docker_ps_table.SetSize()
+	docker_version_box := ui.NewPar(getDockerVersion())
+	docker_version_box.Width = ui.TermWidth()
+	docker_version_box.Height = ui.TermHeight()
+	docker_version_box.BorderLabel = "Docker Information"
+	docker_version_box.BorderFg = ui.ColorCyan
+	docker_version_box.BorderLabelFg = ui.ColorWhite
 
 	ui.Body.AddRows(
 		ui.NewRow(
+			ui.NewCol(4, 0, docker_version_box),
 			ui.NewCol(8, 0, docker_ps_table),
 		),
 	)
